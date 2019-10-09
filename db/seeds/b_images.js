057b8939d4831abc3079d42fcd6390450e64bd10
Matt Raetz <raetzmat@gmail.com>
2019-10-08 23:25:39 -0500

    Fixed S3 URL's in seeding script. Added some styling and started making static placeholder for FE

--- db/seeds/b_images.js
+++ db/seeds/b_images.js
@@ -4,16 +4,17 @@ var faker = require('faker');
 var productImages = function(knex, imgCnt, id) {
   //Variable for set of S3 stored images
   var imageList = [
-    {img_small: 's3://fec-product-images/s-l64.jpg', img_large: 's3://fec-product-images/s-l500.jpg', img_zoom: 's3://fec-product-images/s-l1600.jpg'},
-    {img_small: 's3://fec-product-images/s-l64 (1).jpg', img_large: 's3://fec-product-images/s-l500 (1).jpg', img_zoom: 's3://fec-product-images/s-l1600 (1).jpg'},
-    {img_small: 's3://fec-product-images/s-l64 (2).jpg', img_large: 's3://fec-product-images/s-l500 (2).jpg', img_zoom: 's3://fec-product-images/s-l1600 (2).jpg'},
-    {img_small: 's3://fec-product-images/s-l64 (3).jpg', img_large: 's3://fec-product-images/s-l500 (3).jpg', img_zoom: 's3://fec-product-images/s-l1600 (3).jpg'},
-    {img_small: 's3://fec-product-images/s-l64 (4).jpg', img_large: 's3://fec-product-images/s-l500 (4).jpg', img_zoom: 's3://fec-product-images/s-l1600 (4).jpg'},
-    {img_small: 's3://fec-product-images/s-l64 (5).jpg', img_large: 's3://fec-product-images/s-l500 (5).jpg', img_zoom: 's3://fec-product-images/s-l1600 (5).jpg'},
-    {img_small: 's3://fec-product-images/s-l64 (6).jpg', img_large: 's3://fec-product-images/s-l500 (6).jpg', img_zoom: 's3://fec-product-images/s-l1600 (6).jpg'},
-    {img_small: 's3://fec-product-images/s-l64 (7).jpg', img_large: 's3://fec-product-images/s-l300.jpg', img_zoom: 's3://fec-product-images/s-l1600 (7).jpg'},
-    {img_small: 's3://fec-product-images/s-l64 (8).jpg', img_large: 's3://fec-product-images/s-l300 (1).jpg', img_zoom: 's3://fec-product-images/s-l1600 (8).jpg'},
-    {img_small: 's3://fec-product-images/s-l64 (9).jpg', img_large: 's3://fec-product-images/s-l300 (2).jpg', img_zoom: 's3://fec-product-images/s-l1600 (9).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64.jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500.jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600.jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(1).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(1).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(1).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(2).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(2).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(2).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(3).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(3).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(3).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(4).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(4).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(4).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(5).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(5).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(5).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(6).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(6).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(6).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(7).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l300+(1).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(7).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(8).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(2).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(8).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(9).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(3).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(9).jpg'},
+    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(10).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(4).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(10).jpg'}
   ];
 
   var result = imageList.slice(0, imgCnt);
